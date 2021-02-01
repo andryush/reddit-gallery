06ce625f891c7cf919b7ab0c70850cd61d367da7
andryush <andryush333@gmail.com>
2021-02-01 17:07:15 +0400

    refactored folder structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Header from "../Header/Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="https://www.google.com">
+          Reddit gallery
+        </a>
+      </nav>
+    </div>
+  );
+}
+export default Header;
